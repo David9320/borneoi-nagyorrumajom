0626fe7f4fa6bbe5ff6ed9394c8632fdd5eebb54
Dave <154882041+David9320@users.noreply.github.com>
2026-02-19 23:18:27 +0100

    Update idojaras.js

--- javascript/idojaras.js
+++ javascript/idojaras.js
@@ -1,14 +1,17 @@
 const apiKey ="e70efa4cccaabfd2ae35e27962ed973d";
+let hely = "Samarinda";
 
 async function getIdojaras(varos) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${varos}&appid=${apiKey}&units=metric`);
-    const {main: main, weather: [weather]} = await response.json();
+    const data = await response.json();
+    console.log(data);
+    const {main: main, weather: [weather]} = data;
     return {main: main, weather: weather}
 }
 
 async function setDiv(varos) {
     const {main: main, weather: idojaras} = await getIdojaras(varos);
-    console.log(main);
+    //console.log(await getIdojaras(varos));
     const idojarasDiv = document.getElementById("idojarasDiv");
     idojarasDiv.innerHTML = "";
     addElement("h1", varos);
@@ -26,7 +29,35 @@ function addElement(tipus, szoveg, classText) {
     idojarasDiv.append(elem);
 }
 
-setDiv("Borneo");
+
+
+async function getVarosLista() {
+    const response = await fetch("../adatok/varosok.txt");
+    const data = await response.text();
+    const dataLista = data.split("\t");
+    const varosok = [];
+    for(let i = 1; i < dataLista.length; i += 5) varosok.push(dataLista[i]);
+    return varosok;
+}
+
+async function setupSelect() {
+    const varosSelect = document.getElementById("varosSelect");
+    const varosok = await getVarosLista();
+    varosSelect.addEventListener("input", function() {
+        hely = varosSelect.value;
+        setDiv(hely);
+    });
+    for(let i = 0; i < varosok.length; i++) {
+        const opcio = document.createElement("option");
+        opcio.value = varosok[i];
+        opcio.textContent = varosok[i];
+        varosSelect.append(opcio);
+    }
+}
+
+setupSelect();
+
+setDiv(hely);
 setInterval(function() {
-    setDiv("Borneo");
+    setDiv(hely);
 }, 300000);
